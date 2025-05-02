a9f528c343d06e721810ef6664dbb93af3467f06
CalBlitz <gaurav_sharan@berkeley.edu>
2025-05-02 19:19:35 +0000

    Udated fine_tuner to save the result in a csv

--- src/deep_learning/fine_tuner.py
+++ src/deep_learning/fine_tuner.py
@@ -457,7 +457,7 @@ class FineTuner:
             keys=eval_results.keys(),
             names=["Domain", "Metric"]
         )
-        combined_csv_path = f"{"/experiments/results/", self.method_name}_{self.model_name}_combined_report.csv"
+        combined_csv_path = f"/home/ec2-user/madde/experiments/results/_{self.method_name}_{self.model_name}_combined_report.csv"
         combined_report.to_csv(combined_csv_path)
         print(f"Saved combined evaluation report to {combined_csv_path}")
 
